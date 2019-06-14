58c5ec6ad5f2c895a33f7cc81e650a179c743d49
maxdavid <me@maxdavid.com>
2019-06-14 08:51:39 -0700

    Add GET and POST for building data

--- village/src/components/SmurfForm.js
+++ village/src/components/SmurfForm.js
@@ -1,9 +1,11 @@
 import React, { Component } from 'react';
+import axios from 'axios';
 
 class SmurfForm extends Component {
   constructor(props) {
     super(props);
     this.state = {
+      id: 0,
       name: '',
       age: '',
       height: ''
@@ -12,42 +14,48 @@ class SmurfForm extends Component {
 
   addSmurf = event => {
     event.preventDefault();
-    // add code to create the smurf using the api
+
+    this.setState({ id: Date.now() });
+    axios.post('http://localhost:3333/smurfs', this.state);
 
     this.setState({
+      id: 0,
       name: '',
       age: '',
       height: ''
     });
-  }
+  };
 
   handleInputChange = e => {
-    this.setState({ [e.target.name]: e.target.value });
+    let value = e.target.value;
+    if (value && e.target.name === 'age') value = parseInt(value, 10);
+
+    this.setState({ [e.target.name]: value });
   };
 
   render() {
     return (
-      <div className="SmurfForm">
+      <div className='SmurfForm'>
         <form onSubmit={this.addSmurf}>
           <input
             onChange={this.handleInputChange}
-            placeholder="name"
+            placeholder='name'
             value={this.state.name}
-            name="name"
+            name='name'
           />
           <input
             onChange={this.handleInputChange}
-            placeholder="age"
+            placeholder='age'
             value={this.state.age}
-            name="age"
+            name='age'
           />
           <input
             onChange={this.handleInputChange}
-            placeholder="height"
+            placeholder='height'
             value={this.state.height}
-            name="height"
+            name='height'
           />
-          <button type="submit">Add to the village</button>
+          <button type='submit'>Add to the village</button>
         </form>
       </div>
     );
